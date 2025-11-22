606dc6e932bca902b1ba7c74eeec4ca49de98d3e
Enesssk <140720772+Enesssk@users.noreply.github.com>
2025-11-22 19:57:39 +0300

    Initial commit

---
+++ App.js
@@ -0,0 +1,15 @@
+import { SafeAreaView, Text } from 'react-native';
+
+
+const App = () => {
+
+  return (
+    <SafeAreaView>
+      <Text>Hello World!</Text>
+    </SafeAreaView>
+  );
+}
+
+
+
+export default App;
